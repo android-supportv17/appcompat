759cf9411b83aa9f9cf99fe3182a1ddd7fa95450
android-supportv17 <781334486@qq.com>
2019-03-18 11:18:24 +0800

    second commit

--- app/src/main/java/cn/anhui/mobile/carpooling/MainActivity.java
+++ app/src/main/java/cn/anhui/mobile/carpooling/MainActivity.java
@@ -2,6 +2,7 @@ package cn.anhui.mobile.carpooling;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.supports.v12.AppCompat;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompat.init(this);
     }
 }
